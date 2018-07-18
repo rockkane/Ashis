b6611f49c7ff7df13a59bbf63e21dd09baaaf429
Your Name <you@example.com>
2018-07-18 18:20:57 +0600

    first commit

---
+++ MVCTutorial/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using MVCTutorial.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTutorial.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            MVCTutorialEntities db = new MVCTutorialEntities();
+            List<Employee> employeelist = db.Employees.ToList();
+
+            EmployeeViewModel employeeVM = new EmployeeViewModel();
+
+            List<EmployeeViewModel> employeeVMlist = employeelist.Select(x => new EmployeeViewModel
+            { Name = x.Name,
+                Address = x.Address,
+                DepartmentId = x.DepartmentId,
+                EmployeeId = x.EmployeeId,
+                DepartmentName=x.Department.DepartmentName}).ToList();
+
+            return View(employeeVMlist);
+        }
+    }
+}
---
+++ MVCTutorial/Models/EmployeeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTutorial.Models
+{
+    public class EmployeeViewModel
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public Nullable<int> DepartmentId { get; set; }
+        public string Address { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
